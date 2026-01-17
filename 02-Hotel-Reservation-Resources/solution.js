447d50b04926998ac3b728eed162042663fe2ef6
MimsPeeva <mpeeva04@abv.bg>
2026-01-17 17:13:29 +0200

    Implemented Confirm Reservation form functionality

--- 02-Hotel-Reservation-Resources/solution.js
+++ 02-Hotel-Reservation-Resources/solution.js
@@ -85,3 +85,21 @@ function findRoom(e) {
 }
 
 // Kalin Kolev Endblock
+
+// <!-- Mims Peeva -->
+
+document.querySelector('#confirm-back-btn').addEventListener('click', (e) => getBackToPersonalData(e));
+
+function getBackToPersonalData(e) {
+    e.preventDefault();
+    changeContent('guest-details-form-content');
+}
+
+<></>
+document.querySelector('#confirm-reservation').addEventListener('click', (e) => showThanksPage(e));
+
+function showThanksPage(e) {
+    e.preventDefault();
+    changeContent('thank-you-content');
+}
+// <!-- Mims Peeva -->
